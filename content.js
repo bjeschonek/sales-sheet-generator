0436763cb66c09b5b65a57c348e430b3212e0402
Brad Jeschonek <blj747@gmail.com>
2024-10-24 22:56:42 -0400

    Removed comments from content.js

--- content.js
+++ content.js
@@ -2,16 +2,14 @@
 function getDataFromPage() {
     const data = {};
   
-    // Replace the selectors below with actual selectors from the VinSolutions page
     data.customerName = document.querySelector('.customer-name')?.innerText || '';
     data.vehicleModel = document.querySelector('.vehicle-model')?.innerText || '';
     data.purchaseDate = document.querySelector('.purchase-date')?.innerText || '';
-    // Add more fields as necessary
   
     return data;
   }
   
-  // Listen for messages from the popup
+  // Listen for messages from the popup.js
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'getData') {
       const data = getDataFromPage();
